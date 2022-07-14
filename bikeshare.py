250ea9f49a7dd1135e458b6a2dc64f47e2a0175c
Rapelang Sekilo <rapelangsekilo@gmail.com>
2022-07-14 14:40:21 -0700

    adding a function to display indivual user data

--- bikeshare.py
+++ bikeshare.py
@@ -170,6 +170,15 @@ def user_stats(df):
         print("*"*32)
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+    
+def display_data(df):
+    view_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no\n')
+    start_loc = 0
+    while (view_data == "yes"):
+        print(df.iloc[0:5, :])
+        start_loc += 5
+        view_data = input("Do you wish to continue?: ").lower()
+print('_'*40)
 
 def main():
     while True:
@@ -180,6 +189,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
+        display_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
